ac1a9c4aacb6ba8ef7bbe7c46aaf14331a3bb577
HamzaRasheed26 <hamzarasheed19961@gmail.com>
2023-11-16 22:56:46 +0500

    Add database connection

--- pakcars-node-app/server.js
+++ pakcars-node-app/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 // const userRoutes = require("./routes/userRoutes");
 // const authValidate = require("./utils/authorization_middleware");
-// require("./utils/db");
+require("./utils/db");
 port = 3005;
 
 const app = express();
@@ -13,7 +13,6 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("PakCars API");
 });
-l;
 
 app.listen(port, () => {
   console.log(`Server is runing at port:${port}`);
